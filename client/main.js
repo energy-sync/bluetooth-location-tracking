69debbd282ce33a1f56212f18b18ec60ff33227f
Michael-Trotter <trotmd01@pfw.edu>
2022-10-14 14:46:07 -0400

    simple creation of database entry

--- client/main.js
+++ client/main.js
@@ -2,21 +2,21 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
+import '../lib/database.js';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+Template.insertData.events({
+  'click .js-save'(event,instance){
+    var patientName = $('#pName').val();
+    var location = $('#location').val();
+    var time = Date.now();
+
+    patientInformationdb.insert({
+      "patientName": patientName,
+      "location" : location,
+      "time" : time
+    });
+    $('#pName').val('');
+    $('#location').val('');
+  }
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
