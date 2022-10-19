8909518061fc7970f820f74eab5ef4e74703641a
Dawson Vilamaa <42476104+dawson-vilamaa@users.noreply.github.com>
2022-10-19 18:01:23 -0400

    Added routing using FlowRouter

--- client/main.js
+++ client/main.js
@@ -1,22 +1,25 @@
+import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
-
-import './main.html';
 import '../lib/database.js';
+import '../lib/routing';
+import './pages/main.html';
+import './stylesheets/bootstrap.min.css'
+import "./pages/device-list.html"
 
+//MongoDB
 Template.insertData.events({
-  'click .js-save'(event,instance){
-    var patientName = $('#pName').val();
-    var location = $('#location').val();
-    var time = Date.now();
+    'click .js-save'(event, instance) {
+        var patientName = $('#pName').val();
+        var location = $('#location').val();
+        var time = Date.now();
 
-    patientInformationdb.insert({
-      "patientName": patientName,
-      "location" : location,
-      "time" : time
-    });
-    $('#pName').val('');
-    $('#location').val('');
-  }
+        patientInformationdb.insert({
+            "patientName": patientName,
+            "location": location,
+            "time": time
+        });
+        $('#pName').val('');
+        $('#location').val('');
+    }
 });
-
