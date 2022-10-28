a834998d0770138ca54d7fe0a689db8bd9e71c50
Brandon2532 <pruib01@pfw.edu>
2022-10-28 01:16:37 -0400

    added all the lab info

--- client/main.js
+++ client/main.js
@@ -6,6 +6,7 @@ import './main.html';
 import './stylesheets/bootstrap.min.css';
 import "./pages/device-list.html";
 import "./scripts/device-list";
+import "isomorphic-fetch";
 
 //MongoDB
 Template.insertData.events({
@@ -16,9 +17,53 @@ Template.insertData.events({
             var age = "XX".replace(/X/g, function () {
                 return "0123456789".charAt((Math.random()*10))}) ;
 
-               
-               
+                var DOB = "0X/2X/19XX".replace(/X/g, function () {
+                return "123456789".charAt((Math.random()*9))}) ;
+
+                var bloodPressure = "1XX/XX".replace(/X/g, function () {
+                return "123456789".charAt((Math.random()*9))}) ;
+
+                var heartRate = "XX".replace(/X/g, function () {
+                return "123456789".charAt((Math.random()*9))}) ;
+
+                var RBC = "X.XX M/UL".replace(/X/g, function () {
+                return "123456789".charAt((Math.random()*9))}) ;
+
+                var bloodGlucose = "XXX mg/dl".replace(/X/g, function () {
+                return "123".charAt((Math.random()*3))}) ;
+
+                var hemoglobin = "1X.X g/dl".replace(/X/g, function () {
+                return "0123456789".charAt((Math.random()*10))}) ;
+
+                var hematocrit = "1X.X %".replace(/X/g, function () {
+                return "123456789".charAt((Math.random()*9))}) ;
+
+                var MCV = "XX fL".replace(/X/g, function () {
+                return "789".charAt((Math.random()*3))}) ;
+
+                var plateletCount = "XXX K/dl".replace(/X/g, function () {
+                return "0123456789".charAt((Math.random()*10))}) ;
+
+                var WBC = "XX.X K/uL".replace(/X/g, function () {
+                return "0123456789".charAt((Math.random()*10))}) ;
+
+                var sodium = "1XX mEq/L".replace(/X/g, function () {
+                return "0123456789".charAt((Math.random()*10))}) ;
+
+                var potassium = "X.X mEq/L".replace(/X/g, function () {
+                return "0123456789".charAt((Math.random()*10))}) ;
+                        
+                    
+                
+            
+        
+    
+
               
+
+
+
+
                    
                
                
@@ -30,14 +75,12 @@ Template.insertData.events({
         });
         var patientName = $('#pName').val();
         var address = $('#address').val();
-     //   var DOB = $('#DOB').val();
         var physicanName = $('#physicanName').val();
-      //  var bloodPressure=$('#bloodPressure').val();
-        var heartRate=$('#heartRate').val();
-        var labInfo=$('#labInfo').val();
         var prescriptions=$('#prescriptions').val();
 
+       
         patientInformationdb.insert({
+         
             "macAddress": macAddress,
             "patientName": patientName,
             "address": address,
@@ -47,16 +90,21 @@ Template.insertData.events({
             "physicanName":physicanName,
             "bloodPressure":bloodPressure,
             "heartRate":heartRate,
-            "labInfo":labInfo,
+            "RBC":RBC,
+            "Blood Glucose Level":bloodGlucose,
+            "Hemoglobin": hemoglobin,
+            "Hematocrit": hematocrit,
+            "MCV": MCV,
+            "WBC": WBC,
+            "Sodium":sodium,
+            "Potassium":potassium,
+            "Platelet Count":plateletCount,
             "prescriptions":prescriptions,
             
         });
         $('#pName').val('');
         $('#address').val('');
-      //  $('#DOB').val('');
         $('#physicanName').val('');
-       // $('#bloodPressure').val('');
-        $('#heartRate').val('');
         $('#labInfo').val('');
         $('#prescriptions').val('');
     },
