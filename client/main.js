a2457219bc4de9be04712518d0b87b04e7f26dd9
Dawson Vilamaa <42476104+dawson-vilamaa@users.noreply.github.com>
2022-11-01 16:39:36 -0400

    Start of dynamic patient page

--- client/main.js
+++ client/main.js
@@ -1,6 +1,4 @@
-import { Meteor } from 'meteor/meteor'
 import { Template } from 'meteor/templating';
-import { patientInformationdb } from '../lib/database.js';
 import '../lib/routing';
 import './main.html';
 import './stylesheets/bootstrap.min.css';
@@ -8,6 +6,8 @@ import "./pages/patient-list.html";
 import "./scripts/patient-list.js";
 import "./pages/patient.html";
 import "./scripts/patient.js";
-import ".stylesheets/main.css";
-import ".stylesheets/bootstrap.min.css";
-import ".stylesheets/device-list.css";
+import "./stylesheets/main.css";
+import "./stylesheets/bootstrap.min.css";
+import "./stylesheets/device-list.css";
+
+Template.vitals.inheritsHelpersFrom("patient");
--- client/scripts/patient.js
+++ client/scripts/patient.js
@@ -1,99 +1,30 @@
 import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import { patientInformationdb } from '../../lib/database.js';
+import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
 
-import './patient.html';
-
-//Template.patient.onCreated(function patientOnCreated(){
-    //Placeholder name assignment
-    //this.patientName = new ReactiveVar("John Doe");
-//});
-
-Template.patient.helpers({
-    patientName(){
-        return Template.instance().patientName.get();
-    },
-}
-
-);
-
-Template.patientInformation.onCreated(function patinentInformationCreated(){
-    selectedDepartment = new ReactiveVar("reception")
+Template.patient.onCreated(function() {
+    this.showVitals = new ReactiveVar(false);
 });
 
-Template.patientInformation.helpers({
-    infoSelect: function(){
-        return selectedDepartment;
+Template.patient.helpers({
+    patient() {
+        return patientInformationdb.findOne({"patientInformation.patientID": FlowRouter.getParam("patientID")});
+    },
+    isShowingVitals() {
+        return Template.instance().showVitals.get();
     }
 });
 
-departments.onchange(function departmentsChanged(){
-    selectedDepartment.set(departments.value);
+Template.patient.events({
+    "change #departments": (event, templateInstance) => {
+        switch (event.currentTarget.value) {
+            case "reception":
+                templateInstance.showVitals.set(false);
+                break;
+            case "practitioner":
+                templateInstance.showVitals.set(true);
+                break;
+            
+        }
+    }
 });
-/*
-Template.patientInfo1.onCreated(function patientInfo1OnCreated(){
-    //Placeholder info assignment
-    this.patientLocation = new ReactiveVar("Not Checked In");
-    this.patientPhone = new ReactiveVar("Not registered");
-    this.patientEmail = new ReactiveVar("Not registered");
-    this.patientAge = new ReactiveVar("Not registered");
-
-    this.patientGender = new ReactiveVar("Not registered");
-    this.patientInsurance = new ReactiveVar("Not registered");
-    this.patientPolicy = new ReactiveVar("N/A");
-    this.patientStatus = new ReactiveVar("Not Enrolled");
-
-    this.patientDevice = new ReactiveVar("Not assigned");
-   // patientFamilyHistory: [
-        //this.patientFamilyCondition = new ReactiveVar("None"),
-    //];
-});
-
-Template.patientInfo1.helpers({
-    patientLocation(){
-        return Template.instance().patientLocation.get();
-    },
-
-    patientPhone(){
-        return Template.instance().patientPhone.get();
-    },
-
-    patientEmail(){
-        return Template.instance().patientEmail.get();
-    },
-
-    patientAge(){
-        return Template.instance().patientAge.get();
-    },
-
-    patientGender(){
-        return Template.instance().patientGender.get();
-    },
-
-    patientInsurance(){
-        return Template.instance().patientInsurance.get();
-    },
-
-    patientPolicy(){
-        return Template.instance().patientPolicy.get();
-    },
-
-    patientStatus(){
-        return Template.instance().patientStatus.get();
-    },
-
-    patientDevice(){
-        return Template.instance().patientDevice.get();
-    },
-
-    patientFamilyHistory: [
-        {patientFamilyCondition: "none"}
-    ],
-
-    patientMedication: [
-        {currentMed: "none"}
-    ]
-
-
-
-})
-*/
