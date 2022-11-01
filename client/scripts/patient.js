b8ef5472b4870987fc4115230255d18f75c57089
Dawson Vilamaa <42476104+dawson-vilamaa@users.noreply.github.com>
2022-11-01 16:55:19 -0400

    Added routing and combined the two pages

--- client/scripts/patient.js
+++ client/scripts/patient.js
@@ -1,7 +1,6 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
-import './patient.html';
 
 Template.patient.onCreated(function patientOnCreated(){
     //Placeholder name assignment
