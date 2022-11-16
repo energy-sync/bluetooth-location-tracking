46a0ead24c7d4977454ff6ab6be66297185e1fc4
Michael-Trotter <trotmd01@pfw.edu>
2022-11-16 09:52:05 -0500

    added device db into this branch

--- device-backend/server/main.js
+++ device-backend/server/main.js
@@ -1,19 +1,50 @@
 import { Meteor } from 'meteor/meteor';
 import { WebApp } from 'meteor/webapp';
-import {deviceInformationdb} from '../lib/database.js';
+import { deviceInformationdb } from '../lib/database.js';
 
 Meteor.startup(() => {
   // code to run on server at startup
-});
+  deviceInformationdb.remove({});
 
-WebApp.connectHandlers.use('/', (req, res) =>{
-  if(req.method === 'POST'){
-    res.writeHead(200)
-    let data = Object.values(JSON.parse(req.body))
-    deviceInformationdb.insert({
-      'patientID':data[0],
-      'deviceID' : data[1]
-    })
-    res.end()
+  const locations = ["Reception", "Dermatology", "General Practitioner", "Lab"];
+
+  //random number
+  const getRandomNumber = (max) => Math.floor(Math.random() * max);
+
+
+  //getting random names
+  function getRandomLocation(arr1) {
+    let location = arr1[getRandomNumber(arr1.length)];
+    return location;
   }
-})
+
+
+  for (let i = 0; i < 6; i++) {
+    var patientID = "XXXXXXX".replace(/X/g, function () {
+      return "0123456789ABCDEF".charAt((Math.random() * 16))
+    });
+    var macAddress = "XX:XX:XX:XX:XX:XX".replace(/X/g, function () {
+      return "0123456789ABCDEF".charAt((Math.random() * 16))
+    });
+    function getTimestampInSeconds() {
+      return Math.floor(Date.now() / 1000)
+    }
+
+
+    var deviceID = i;
+
+    var timeStamp = getTimestampInSeconds();
+
+    var location = getRandomLocation(locations);
+
+    deviceInformationdb.insert({
+
+      "deviceID": deviceID,
+      "macAddress": macAddress,
+      "location": location,
+      "patientID": patientID,
+      "timeStamp": timeStamp,
+    });
+
+  }
+});
