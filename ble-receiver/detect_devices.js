4b228ee6e16de9188be8145310b03e034d47037b
Dawson Vilamaa <electronsync@gmail.com>
2022-11-29 23:54:49 -0500

    First version of receiver program

--- ble-receiver/detect_devices.js
+++ ble-receiver/detect_devices.js
@@ -1,5 +1,6 @@
 const BeaconScanner = require('node-beacon-scanner');
 const scanner = new BeaconScanner();
+import axiox from "axios";
 
 let devices = {};
 let oldDevices;
@@ -7,7 +8,7 @@ let oldDevices;
 scanner.onadvertisement = ad => {
 	if (!devices[ad.address]) {
 		oldDevices = devices;
-		devices[ad.address] = ad;
+		devices[ad.address] = ad.rssi;
 	}
 	//console.log(JSON.stringify(ad, null, 4));
 };
@@ -22,5 +23,14 @@ setInterval(() => {
 	if (Object.keys(devices).length > 0) {
 		console.log("\nDEVICES\n=======\n");
 		console.log(devices);
+
+		if (oldDevices) {
+			for (mac in devices) {
+				if (oldDevices[mac] && oldDevices[mac] !== devices[mac]) {
+					
+				}
+			}
+		}
+		oldDevices = Object.assign(oldDevices, devices);
 	}
 }, 1000);
