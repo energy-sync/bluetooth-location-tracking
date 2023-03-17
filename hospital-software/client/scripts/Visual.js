12a40dd409a5a2bca11da471db7ed1ddad36fbec
Your Name <bigii01@pfw.edu>
2023-03-17 02:56:40 -0400

    Yes

--- hospital-software/client/scripts/Visual.js
+++ hospital-software/client/scripts/Visual.js
@@ -1,60 +1,11 @@
 import { Template } from 'meteor/templating';
+import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 import './Visual.html';
 import './Visual.css';
 
 Template.Visual.events({
-  'click .btn1': function() {
-    const currentLocation = document.querySelector('.Locator p:first-child');
-    currentLocation.textContent = 'Current location: Reception';
-    showQuestions();
-  },
-
-  'click .btn2': function() {
-    const currentLocation = document.querySelector('.Locator p:first-child');
-    currentLocation.textContent = 'Current location: Lab';
-  },
-
-  'click .btn3': function() {
-    const currentLocation = document.querySelector('.Locator p:first-child');
-    currentLocation.textContent = 'Current location: Practioner';
-  },
-
-  'click .btn4': function() {
-    const currentLocation = document.querySelector('.Locator p:first-child');
-    currentLocation.textContent = 'Current location: Dermatology';
+  'click .reception-btn'(event) {
+    event.preventDefault();
+    BlazeLayout.render('patient', {department: 'reception'});
   }
 });
-
-function showQuestions() {
-  const questions = ['What is your name?', 
-  'What is your date of birth?', 
-  'What is your address?',
-  'What is the reason for your visit?'
-  ];
-  
-  const popup = document.createElement('div');
-  popup.className = 'popup';
-
-  const header = document.createElement('h2');
-  header.textContent = 'Questions';
-
-  const list = document.createElement('ul');
-  questions.forEach(function(question) {
-    const listItem = document.createElement('li');
-    listItem.textContent = question;
-    list.appendChild(listItem);
-  });
-
-  popup.appendChild(header);
-  popup.appendChild(list);
-  document.body.appendChild(popup);
-
-  const closeButton = document.createElement('button');
-  closeButton.textContent = 'Close';
-  popup.appendChild(closeButton);
-
-  closeButton.addEventListener('click', function() {
-    document.body.removeChild(popup);
-  });
-}
-
