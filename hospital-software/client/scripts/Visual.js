e77d16cb7b40e6db446b9aa533e13000b6de61cb
Your Name <bigii01@pfw.edu>
2023-03-27 13:33:40 -0400

    idk pt.ii

--- hospital-software/client/scripts/Visual.js
+++ hospital-software/client/scripts/Visual.js
@@ -1,11 +1,28 @@
 import { Template } from 'meteor/templating';
-import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 import './Visual.html';
-import './Visual.css';
 
-Template.Visual.events({
-  'click .reception-btn'(event) {
-    event.preventDefault();
-    BlazeLayout.render('patient', {department: 'reception'});
-  }
+Template.myTemplate.onCreated(function () {
+  this.title = 'Device Visualization';
+  this.devicesReception = ['Device 1', 'Device 2', 'Device 3'];
+  this.devicesLab = ['Device 4', 'Device 5', 'Device 6'];
+  this.devicesGP = ['Device 7', 'Device 8', 'Device 9'];
+  this.devicesDermatology = ['Device 10', 'Device 11', 'Device 12'];
+});
+
+Template.myTemplate.helpers({
+  title() {
+    return Template.instance().title;
+  },
+  devicesReception() {
+    return Template.instance().devicesReception;
+  },
+  devicesLab() {
+    return Template.instance().devicesLab;
+  },
+  devicesGP() {
+    return Template.instance().devicesGP;
+  },
+  devicesDermatology() {
+    return Template.instance().devicesDermatology;
+  },
 });
