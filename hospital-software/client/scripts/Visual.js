1b70ffa2260981a114c53c00417bc3ae87575673
Your Name <bigii01@pfw.edu>
2023-03-28 14:58:44 -0400

    Finally something

--- hospital-software/client/scripts/Visual.js
+++ hospital-software/client/scripts/Visual.js
@@ -1,28 +1,16 @@
-import { Template } from 'meteor/templating';
-import './Visual.html';
-
-Template.myTemplate.onCreated(function () {
-  this.title = 'Device Visualization';
-  this.devicesReception = ['Device 1', 'Device 2', 'Device 3'];
-  this.devicesLab = ['Device 4', 'Device 5', 'Device 6'];
-  this.devicesGP = ['Device 7', 'Device 8', 'Device 9'];
-  this.devicesDermatology = ['Device 10', 'Device 11', 'Device 12'];
+Template.Visual.helpers({
+  deviceLab() {
+    return Template.instance().deviceLab.get();
+  },
+  deviceReception() {
+    return Template.instance().deviceReception.get();
+  },
+  deviceGP() {
+    return Template.instance().deviceGP.get();
+  },
+  deviceDermatology() {
+    return Template.instance().deviceDermatology.get();
+  }
+  
 });
 
-Template.myTemplate.helpers({
-  title() {
-    return Template.instance().title;
-  },
-  devicesReception() {
-    return Template.instance().devicesReception;
-  },
-  devicesLab() {
-    return Template.instance().devicesLab;
-  },
-  devicesGP() {
-    return Template.instance().devicesGP;
-  },
-  devicesDermatology() {
-    return Template.instance().devicesDermatology;
-  },
-});
