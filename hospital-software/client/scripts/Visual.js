266068cc438b859d2dcc2677100ebcf138bff1bf
Your Name <bigii01@pfw.edu>
2023-04-18 13:50:33 -0400

    Ps

--- hospital-software/client/scripts/Visual.js
+++ hospital-software/client/scripts/Visual.js
@@ -1,27 +1,31 @@
-import{Template} from 'meteor/templating'
-import {ReactiveVar} from 'meteor/reactive-var'
-
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+import '/Visual.html';
+import '/Visual.css';
+import { patientInformationdb } from '../../lib/database';
 
 Template.Visual.onCreated(function() {
-  this.reception=new ReactiveVar([])
-  this.dermatology=new ReactiveVar([])
-  this.lab=new ReactiveVar([])
-  this.practitioner=new ReactiveVar([])
+  this.reception = new ReactiveVar([]);
+  this.dermatology = new ReactiveVar([]);
+  this.lab = new ReactiveVar([]);
+  this.practitioner = new ReactiveVar([]);
 
-  Meteor.call("getLocationPatient", "Receptionist",(err, res) =>{
-    this.reception.set(res)
-  })
+  Meteor.call("getLocationPatient", "Receptionist", (err, res) => {
+    this.reception.set(res);
+  });
 
-  Meteor.call("getLocationPatient", "Dermatology",(err, res) =>{
-    this.dermatology.set(res)
-  })
-  Meteor.call("getLocationPatient", "Lab",(err, res) =>{
-    this.lab.set(res)
-  })
-  Meteor.call("getLocationPatient", "General Practitioner",(err, res) =>{
-    this.practitioner.set(res)
-  })
-})
+  Meteor.call("getLocationPatient", "Dermatology", (err, res) => {
+    this.dermatology.set(res);
+  });
+
+  Meteor.call("getLocationPatient", "Lab", (err, res) => {
+    this.lab.set(res);
+  });
+
+  Meteor.call("getLocationPatient", "General Practitioner", (err, res) => {
+    this.practitioner.set(res);
+  });
+});
 
 Template.Visual.helpers({
   deviceLab() {
@@ -36,6 +40,45 @@ Template.Visual.helpers({
   deviceDermatology() {
     return Template.instance().dermatology.get();
   }
-  
 });
+
+Template.Visual.onRendered(function() {
+  const green = "#32CD32";
+  const yellow = "#FFFF00";
+  const red = "#FF0000";
+  const timeThreshold1 = 30 * 60 * 1000; // 30 minutes
+  const timeThreshold2 = 60 * 60 * 1000; // 60 minutes
+  const allDevices = document.querySelectorAll(".rounded-circle");
+
+  function setDeviceColor(device, waitingTime) {
+    if (waitingTime > timeThreshold2) {
+      device.classList.remove('green', 'yellow');
+      device.classList.add('red');
+    } else if (waitingTime > timeThreshold1) {
+      device.classList.remove('green', 'red');
+      device.classList.add('yellow');
+    } else {
+      device.classList.remove('yellow', 'red');
+      device.classList.add('green');
+    }
+  }
+  
+  function updateTime() {
+    allDevices.forEach(device => {
+      let beacon= patientInformationdb.findOne({'beaconID': device})
+      const waitingTime = moment(beacon.timeOfUpdate).fromNow()
+      console.log(beacon,waitingTime)
+      setDeviceColor(device, waitingTime);
+    });
+  }
+
+  function startUpdatingTime() {
+    setInterval(updateTime, 5000); // update every 5 seconds
+    setInterval(updateTime, 10000); // update every 10 seconds
+    setInterval(updateTime, 15000); // update every 15 seconds
+  }
+
+  startUpdatingTime();
+});
+
 
